9203e65b887f61b50fc820c50f4a1430a50801a0
yuberGC <yuber.gutierrez.mt@correo.usa.edu.co>
2021-10-23 20:06:19 -0500

    servicio cliente update

--- src/main/java/reto/api/econtrolador/ControladorCliente.java
+++ src/main/java/reto/api/econtrolador/ControladorCliente.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -49,6 +50,12 @@ public class ControladorCliente {
     public Cliente save(@RequestBody Cliente client) {
         return servicio.save(client);
     }   
+    
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente client) {
+        return servicio.update(client);
+    }
     
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
